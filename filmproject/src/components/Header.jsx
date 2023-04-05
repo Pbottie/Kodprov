39afc3591322427b715b380ca8eae8ad93f614b8
Kaami <pbottie@gmail.com>
2023-04-05 15:19:54 +0200

    Probably done

--- filmproject/src/components/Header.jsx
+++ filmproject/src/components/Header.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import { React, useState } from "react";
 import "./Header.css";
 
 function Header() {
+  const [isShowing, setIsShowing] = useState(false);
+
+  function toggleMenu() {
+    isShowing ? setIsShowing(false) : setIsShowing(true);
+  }
+
   return (
     <div className="header">
       <h1>DEMO Streaming</h1>
-      <div className="links">
+      <button className="hamburger" onClick={toggleMenu}>
+        <span className="material-symbols-outlined">menu</span>
+      </button>
+      <div className={isShowing ? "links expanded" : "links"}>
         <button>
           <a href=""> Log In </a>
         </button>
